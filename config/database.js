48cf749dfcffa52ff60fa166b0c99854ef7952f1
Diogo Felde <diogofelde@gmail.com>
2025-09-26 18:04:24 -0300

    prepare deploy: add runtime debug and validate config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
-// config/database.js
+﻿// config/database.js
 
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log('DATABASE_URL_PRESENT:', !!process.env.DATABASE_URL);
+console.log('DATABASE_URL_PREFIX:', (process.env.DATABASE_URL || '').slice(0,40));
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
