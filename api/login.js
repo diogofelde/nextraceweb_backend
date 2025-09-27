3771d48a58cce313c75ee4234c22574da061ce74
Diogo Felde <diogofelde@gmail.com>
2025-09-26 21:28:26 -0300

    Atualiza backend: .env, .gitignore e ajustes finais

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
 app.post('/api/login', express.json(), (req, res) => {
     const { username, password } = req.body;
-    if (username === 'admin' && password === '1234') {
+    if (username === 'Diogo' && password === 'D1090') {
         return res.json({ token: 'fake-jwt-token' });
     }
     res.status(401).json({ erro: 'Credenciais inválidas' });
