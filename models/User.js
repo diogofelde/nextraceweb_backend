d076d22eca218b93ae9b3dafc92bff00186a697a
Diogo Felde <diogofelde@gmail.com>
2025-09-27 18:35:26 -0300

    🔧 Atualizações no backend: conexão com banco e ajustes

--- models/User.js
+++ models/User.js
@@ -2,43 +2,10 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
 
 const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    matricula: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    password_hash: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    team: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    permissions: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    is_active: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true
-    }
-}, {
-    tableName: 'users',
-    timestamps: true,
-    underscored: true
+    username: DataTypes.STRING,
+    passwordHash: DataTypes.STRING,
+    team: DataTypes.STRING,
+    permissions: DataTypes.JSON
 });
 
 export default User;
